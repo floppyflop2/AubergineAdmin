556e14b31546a6001c96d81b3e9cb998fd3e24a7
babwin <i3abwin@gmail.com>
2017-02-08 17:12:54 +0100

    Partie Etudiante Fini. Pas encore d'impression PDF

--- app/src/main/java/com/example/florian/aubergine/View/MainActivity.java
+++ app/src/main/java/com/example/florian/aubergine/View/MainActivity.java
@@ -9,16 +9,17 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.example.florian.aubergine.Model.UtilisateurModel;
 import com.example.florian.aubergine.MySingleton;
 import com.example.florian.aubergine.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String url = "http://fierce-basin-74883.herokuapp.com/api";
-    UtilisateurModel mModel;
+    //UtilisateurModel mModel;
     //DalCommunication dalCommunication = new DalCommunication(url);
     //private UserControllerImplementation cController;
 
@@ -43,24 +44,47 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-            final TextView mTxtDisplay;
-
-            mTxtDisplay = (TextView) findViewById(R.id.identifiant);
-            String url = "http://puu.sh/tTGLK/bd1cefcef0.json";
-
+            final TextView erreur = (TextView) findViewById(R.id.erreur);
+            final TextView matricule = (TextView) findViewById(R.id.matricule);
+            //String url = "https://puu.sh/tTPpB/4a50a9429f.json";
+            //String url = "https://puu.sh/";
+            String url= "http://fierce-basin-74883.herokuapp.com/api/login/";
             JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    (Request.Method.GET, url+matricule.getText(), null, new Response.Listener<JSONObject>() {
 
                         @Override
                         public void onResponse(JSONObject response) {
-                            mTxtDisplay.setText("Response: " + response.toString());
+                            setContentView(R.layout.feuille_de_login);
+                            final TextView feuilleDeLoginsTW = (TextView) findViewById(R.id.logins);
+                            try{
+                                JSONArray logiciels = response.getJSONArray("logiciels");
+                                String feuilleDeLogins= "";
+                                for(int i= 0; i<logiciels.length(); i++){
+                                    JSONObject logiciel =logiciels.getJSONObject(i);
+                                    feuilleDeLogins +=logiciel.get("nom")+" :\n";
+                                    feuilleDeLogins +="login : "+logiciel.get("login")+"\n";
+                                    feuilleDeLogins +="mdp : "+logiciel.get("mdp")+"\n\n";
+                                }
+
+
+                                feuilleDeLoginsTW.setText(feuilleDeLogins);
+                            }catch (JSONException e){
+                                erreur.setText("erreur interne, JSON mal construit");
+                                System.out.println(e);
+                            }
                         }
                     }, new Response.ErrorListener() {
 
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            // TODO Auto-generated method stub
 
+                            System.out.print(error);
+                            matricule.setText("");
+                            if(error.networkResponse==null){
+                                erreur.setText("Service Hors Ligne");
+                            }else {
+                                erreur.setText("Matricule inexistant");
+                            }
                         }
                     });
 
