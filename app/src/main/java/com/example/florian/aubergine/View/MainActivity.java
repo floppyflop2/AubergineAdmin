a7d17f974c555d306c9c7784a958c36b776b2cee
babwin <i3abwin@gmail.com>
2017-02-08 14:28:48 +0100

    Get le json du push

--- app/src/main/java/com/example/florian/aubergine/View/MainActivity.java
+++ app/src/main/java/com/example/florian/aubergine/View/MainActivity.java
@@ -3,22 +3,24 @@ package com.example.florian.aubergine.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.toolbox.Volley;
-import com.example.florian.aubergine.Controller.UserController;
-import com.example.florian.aubergine.Controller.UserControllerImplementation;
-import com.example.florian.aubergine.Model.DalCommunication;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.florian.aubergine.Model.UtilisateurModel;
+import com.example.florian.aubergine.MySingleton;
 import com.example.florian.aubergine.R;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String url = "http://fierce-basin-74883.herokuapp.com/api";
     UtilisateurModel mModel;
-    DalCommunication dalCommunication = new DalCommunication(url);
-    private UserControllerImplementation cController;
+    //DalCommunication dalCommunication = new DalCommunication(url);
+    //private UserControllerImplementation cController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.loginButton).setOnClickListener(login);
-        this.cController=new UserControllerImplementation(this,dalCommunication,Volley.newRequestQueue(this));
+        //this.cController=new UserControllerImplementation(this,dalCommunication,Volley.newRequestQueue(this));
 
     }
     @Override
@@ -39,12 +41,31 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             System.out.println("bla ");
 
-            TextView txtV = (TextView) findViewById(R.id.identifiant);
 
-            String authLogin = txtV.getText().toString();
-            UtilisateurModel uM = cController.login(authLogin);
-            txtV.setText(uM.getMatricule());
 
+            final TextView mTxtDisplay;
+
+            mTxtDisplay = (TextView) findViewById(R.id.identifiant);
+            String url = "http://puu.sh/tTGLK/bd1cefcef0.json";
+
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            mTxtDisplay.setText("Response: " + response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // TODO Auto-generated method stub
+
+                        }
+                    });
+
+// Access the RequestQueue through your singleton class.
+            MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsObjRequest);
         }
     };
 
