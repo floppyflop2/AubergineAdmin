a61559b359ad30d35415a63f7233d9e07c637854
Florian <floppy007_flo@hotmail.com>
2017-02-09 10:43:30 +0100

    correction du gradle et ajout import classe R cfr MainActivity

--- app/src/main/java/com/example/florian/aubergine/View/MainActivity.java
+++ app/src/main/java/com/example/florian/aubergine/View/MainActivity.java
@@ -9,6 +9,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.florian.app.R;
 import com.example.florian.aubergine.MySingleton;
 
 
